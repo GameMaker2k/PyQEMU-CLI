d7bf4607e7e5cf78553524292ea47c0163e0abcb
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-10-11 12:21:23 -0500

    Revert changes

--- pyqemu-cli.py
+++ pyqemu-cli.py
@@ -15,49 +15,40 @@
     $FileInfo: qemu-cli.py - Last Update: 4/07/2015 Ver. 0.0.1 RC 1 - Author: cooldude2k $
 '''
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
+import sys
 import re
 import subprocess
-import sys
 
 __project__ = "PyQEMU-CLI"
 __version_info__ = (0, 0, 1, "RC 1")
-if (__version_info__[3] is not None):
-    __version__ = "{major}.{minor}.{build} {release}".format(
-        major=__version_info__[0],
-        minor=__version_info__[1],
-        build=__version_info__[2],
-        release=__version_info__[3])
-if (__version_info__[3] is None):
+if(__version_info__[3] is not None):
+    __version__ = "{major}.{minor}.{build} {release}".format(major=__version_info__[
+                                                             0], minor=__version_info__[1], build=__version_info__[2], release=__version_info__[3])
+if(__version_info__[3] is None):
     __version__ = "{major}.{minor}.{build}".format(
-        major=__version_info__[0],
-        minor=__version_info__[1],
-        build=__version_info__[2])
+        major=__version_info__[0], minor=__version_info__[1], build=__version_info__[2])
 __version_alt__ = "{major}.{minor}.{build}".format(
-    major=__version_info__[0],
-    minor=__version_info__[1],
-    build=__version_info__[2])
+    major=__version_info__[0], minor=__version_info__[1], build=__version_info__[2])
 
 qemuimgexecname = "qemu-img"
 qemu64execname = "qemu-system-x86_64"
 qemu32execname = "qemu-system-i386"
-if (sys.platform == "win32"):
+if(sys.platform == "win32"):
     qemuimgexecname = "qemu-img.exe"
     qemu64execname = "qemu-system-x86_64.exe"
     qemu32execname = "qemu-system-i386.exe"
-if (len(sys.argv) == 1):
+if(len(sys.argv) == 1):
     cmdargpath = os.path.realpath(os.getcwd())
-if (len(sys.argv) > 1):
+if(len(sys.argv) > 1):
     cmdargpath = sys.argv[1]
-    if (not os.path.exists(cmdargpath)):
+    if(not os.path.exists(cmdargpath)):
         cmdargpath = os.path.realpath(os.getcwd())
     cmdargpath = os.path.realpath(cmdargpath)
-    if (not os.path.isdir(cmdargpath)):
+    if(not os.path.isdir(cmdargpath)):
         cmdargpath = os.path.dirname(os.path.realpath(cmdargpath))
-    if (not os.path.exists(cmdargpath)):
+    if(not os.path.exists(cmdargpath)):
         cmdargpath = os.path.realpath(os.getcwd())
 
 os.environ["PATH"] = os.environ["PATH"] + os.pathsep + \
@@ -77,7 +68,7 @@ def listize(varlist):
     newlistreg = {}
     newlistrev = {}
     newlistfull = {}
-    while (il < ix):
+    while(il < ix):
         newlistreg.update({ilx: varlist[il]})
         newlistrev.update({varlist[il]: ilx})
         ilx = ilx + 1
@@ -96,7 +87,7 @@ def twolistize(varlist):
     newlistdescreg = {}
     newlistdescrev = {}
     newlistfull = {}
-    while (il < ix):
+    while(il < ix):
         newlistnamereg.update({ilx: varlist[il][0].strip()})
         newlistnamerev.update({varlist[il][0].strip(): ilx})
         newlistdescreg.update({ilx: varlist[il][1].strip()})
@@ -117,7 +108,7 @@ def arglistize(proexec, *varlist):
     ix = len(varlist)
     ilx = 1
     newarglist = [proexec]
-    while (il < ix):
+    while(il < ix):
         if varlist[il][0] is not None:
             newarglist.append(varlist[il][0])
         if varlist[il][1] is not None:
@@ -131,15 +122,11 @@ qemuimghelplistp = subprocess.Popen(
     [qemuimgllocatout, '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemuimglhelpout, qemuimglhelperr = qemuimghelplistp.communicate()
 qemuimglformatsprelist = re.findall(
-    "Supported formats: ([\\w\\_ ]+)\n",
-    qemuimglhelpout,
-    flags=re.MULTILINE)[0]
+    "Supported formats: ([\w\_ ]+)\n", qemuimglhelpout, flags=re.MULTILINE)[0]
 qemuimglformatslist = qemuimglformatsprelist.split(" ")
 qemuimglformatsnlist = listize(qemuimglformatslist)
 qemuimglversion = re.findall(
-    "^qemu-img version ([\\w\\_\\. \\-]+)\\,",
-    qemuimglhelpout,
-    flags=re.MULTILINE)[0].strip()
+    "^qemu-img version ([\w\_\. \-]+)\,", qemuimglhelpout, flags=re.MULTILINE)[0].strip()
 
 
 qemu32llocatout = which_exec(qemu32execname)
@@ -147,70 +134,50 @@ qemu32helplistp = subprocess.Popen(
     [qemu32llocatout, '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu32lhelpout, qemu32lhelperr = qemu32helplistp.communicate()
 qemu32lverfull = re.findall(
-    "QEMU emulator version ([\\w\\_\\.\\+\\(\\) \\-]+)\\, Copyright",
-    qemu32lhelpout,
-    flags=re.MULTILINE)[0].strip()
+    "QEMU emulator version ([\w\_\.\+\(\) \-]+)\, Copyright", qemu32lhelpout, flags=re.MULTILINE)[0].strip()
 qemu32lversion = re.findall(
-    "([\\w\\_\\. \\-]+)", qemu32lverfull, flags=re.MULTILINE)[0].strip()
+    "([\w\_\. \-]+)", qemu32lverfull, flags=re.MULTILINE)[0].strip()
 qemu32lvgaprelist = re.findall(
-    "vga \\[([\\w\\|]+)\\]\n", qemu32lhelpout, flags=re.MULTILINE)[0]
+    "vga \[([\w\|]+)\]\n", qemu32lhelpout, flags=re.MULTILINE)[0]
 qemu32lvgalist = qemu32lvgaprelist.split("|")
 qemu32lvganlist = listize(qemu32lvgalist)
 qemu32lrtcbaseprelist = re.findall(
-    "\\[base\\=([\\w\\|]+)\\]", qemu32lhelpout, flags=re.MULTILINE)[0]
+    "\[base\=([\w\|]+)\]", qemu32lhelpout, flags=re.MULTILINE)[0]
 qemu32lrtcbaselist = qemu32lrtcbaseprelist.split("|")
 qemu32lrtcbasenlist = listize(qemu32lrtcbaselist)
 qemu32lrtcclockprelist = re.findall(
-    "\\[\\,clock\\=([\\w\\|]+)\\]", qemu32lhelpout, flags=re.MULTILINE)[0]
+    "\[\,clock\=([\w\|]+)\]", qemu32lhelpout, flags=re.MULTILINE)[0]
 qemu32lrtcclocklist = qemu32lrtcclockprelist.split("|")
 qemu32lrtcclocknlist = listize(qemu32lrtcclocklist)
 qemu32lrtcdriftfixprelist = re.findall(
-    "\\[\\,driftfix\\=([\\w\\|]+)\\]", qemu32lhelpout, flags=re.MULTILINE)[0]
+    "\[\,driftfix\=([\w\|]+)\]", qemu32lhelpout, flags=re.MULTILINE)[0]
 qemu32lrtcdriftlist = qemu32lrtcdriftfixprelist.split("|")
 qemu32lrtcdriftnlist = listize(qemu32lrtcdriftlist)
 qemu32cpulistp = subprocess.Popen(
     [qemu32llocatout, '-cpu', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu32lcpuout, qemu32lcpuerr = qemu32cpulistp.communicate()
 qemu32lcpulist = re.findall(
-    "^x86\\s\\s+([\\w]+)  ([\\s]+|[\\w\\(\\)\\.\\@\\_\\/ \\-]+)\n",
-    qemu32lcpuout,
-    flags=re.MULTILINE)
+    "^x86\s\s+([\w]+)  ([\s]+|[\w\(\)\.\@\_\/ \-]+)\n", qemu32lcpuout, flags=re.MULTILINE)
 qemu32lcpunlist = twolistize(qemu32lcpulist)
 qemu32machinelistp = subprocess.Popen(
     [qemu32llocatout, '-machine', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu32lmachineout, qemu32lmachineerr = qemu32machinelistp.communicate()
 qemu32lmachinelist = re.findall(
-    "^([\\w\\-\\.]+)\\s\\s+([\\w\\(\\)\\+\\,\\. \\-]+)\n",
-    qemu32lmachineout,
-    flags=re.MULTILINE)
+    "^([\w\-\.]+)\s\s+([\w\(\)\+\,\. \-]+)\n", qemu32lmachineout, flags=re.MULTILINE)
 qemu32lmachinenlist = twolistize(qemu32lmachinelist)
 qemu32soundhwlistp = subprocess.Popen(
     [qemu32llocatout, '-soundhw', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu32lsoundhwout, qemu32lsoundhwerr = qemu32soundhwlistp.communicate()
 qemu32lsoundhwlist = re.findall(
-    "^([\\w]+)\\s\\s+([\\w\\(\\) ]+)\n", qemu32lsoundhwout, flags=re.MULTILINE)
+    "^([\w]+)\s\s+([\w\(\) ]+)\n", qemu32lsoundhwout, flags=re.MULTILINE)
 qemu32lsoundhwlist.append(('all', 'Enable all sound cards'))
 qemu32lsoundhwnlist = twolistize(qemu32lsoundhwlist)
-qemu32larglist = arglistize(
-    qemu32llocatout,
-    ('-hda',
-     '/dev/null'),
-    ('-cdrom',
-     '/home/cooldude2k/Emu/Minix3/minix_R3.3.0-588a35b.iso'),
-    ('-boot',
-     'order=acd,once=d,menu=on'),
-    ('-cpu',
-     qemu32lcpunlist['name']['reg'][9]),
-    ('-machine',
-     qemu32lmachinenlist['name']['reg'][16]),
-    ('-soundhw',
-     qemu32lsoundhwnlist['name']['reg'][8]),
-    ('-sdl',
-     None))
-print("PATH environment variable is set to " + os.environ["PATH"])
-print("Python " + sys.version)
-print(__project__ + " version " + __version__)
-print("QEMU emulator version " + qemu32lverfull)
+qemu32larglist = arglistize(qemu32llocatout, ('-hda', '/dev/null'), ('-cdrom', '/home/cooldude2k/Emu/Minix3/minix_R3.3.0-588a35b.iso'), ('-boot', 'order=acd,once=d,menu=on'),
+                            ('-cpu', qemu32lcpunlist['name']['reg'][9]), ('-machine', qemu32lmachinenlist['name']['reg'][16]), ('-soundhw', qemu32lsoundhwnlist['name']['reg'][8]), ('-sdl', None))
+print("PATH environment variable is set to "+os.environ["PATH"])
+print("Python "+sys.version)
+print(__project__+" version "+__version__)
+print("QEMU emulator version "+qemu32lverfull)
 print(qemu32larglist)
 qemu32cmdlaunch = subprocess.Popen(
     qemu32larglist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -224,70 +191,50 @@ qemu64helplistp = subprocess.Popen(
     [qemu64llocatout, '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu64lhelpout, qemu64lhelperr = qemu64helplistp.communicate()
 qemu64lverfull = re.findall(
-    "QEMU emulator version ([\\w\\_\\.\\+\\(\\) \\-]+)\\, Copyright",
-    qemu64lhelpout,
-    flags=re.MULTILINE)[0].strip()
+    "QEMU emulator version ([\w\_\.\+\(\) \-]+)\, Copyright", qemu64lhelpout, flags=re.MULTILINE)[0].strip()
 qemu64lversion = re.findall(
-    "([\\w\\_\\. \\-]+)", qemu64lverfull, flags=re.MULTILINE)[0].strip()
+    "([\w\_\. \-]+)", qemu64lverfull, flags=re.MULTILINE)[0].strip()
 qemu64lvgaprelist = re.findall(
-    "vga \\[([\\w\\|]+)\\]\n", qemu64lhelpout, flags=re.MULTILINE)[0]
+    "vga \[([\w\|]+)\]\n", qemu64lhelpout, flags=re.MULTILINE)[0]
 qemu64lvgalist = qemu64lvgaprelist.split("|")
 qemu64lvganlist = listize(qemu64lvgalist)
 qemu64lrtcbaseprelist = re.findall(
-    "\\[base\\=([\\w\\|]+)\\]", qemu64lhelpout, flags=re.MULTILINE)[0]
+    "\[base\=([\w\|]+)\]", qemu64lhelpout, flags=re.MULTILINE)[0]
 qemu64lrtcbaselist = qemu64lrtcbaseprelist.split("|")
 qemu64lrtcbasenlist = listize(qemu64lrtcbaselist)
 qemu64lrtcclockprelist = re.findall(
-    "\\[\\,clock\\=([\\w\\|]+)\\]", qemu64lhelpout, flags=re.MULTILINE)[0]
+    "\[\,clock\=([\w\|]+)\]", qemu64lhelpout, flags=re.MULTILINE)[0]
 qemu64lrtcclocklist = qemu64lrtcclockprelist.split("|")
 qemu64lrtcclocknlist = listize(qemu64lrtcclocklist)
 qemu64lrtcdriftfixprelist = re.findall(
-    "\\[\\,driftfix\\=([\\w\\|]+)\\]", qemu64lhelpout, flags=re.MULTILINE)[0]
+    "\[\,driftfix\=([\w\|]+)\]", qemu64lhelpout, flags=re.MULTILINE)[0]
 qemu64lrtcdriftlist = qemu64lrtcdriftfixprelist.split("|")
 qemu64lrtcdriftnlist = listize(qemu64lrtcdriftlist)
 qemu64cpulistp = subprocess.Popen(
     [qemu64llocatout, '-cpu', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu64lcpuout, qemu64lcpuerr = qemu64cpulistp.communicate()
 qemu64lcpulist = re.findall(
-    "^x86\\s\\s+([\\w]+)  ([\\s]+|[\\w\\(\\)\\.\\@\\_\\/ \\-]+)\n",
-    qemu64lcpuout,
-    flags=re.MULTILINE)
+    "^x86\s\s+([\w]+)  ([\s]+|[\w\(\)\.\@\_\/ \-]+)\n", qemu64lcpuout, flags=re.MULTILINE)
 qemu64lcpunlist = twolistize(qemu64lcpulist)
 qemu64machinelistp = subprocess.Popen(
     [qemu64llocatout, '-machine', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu64lmachineout, qemu64lmachineerr = qemu64machinelistp.communicate()
 qemu64lmachinelist = re.findall(
-    "^([\\w\\-\\.]+)\\s\\s+([\\w\\(\\)\\+\\,\\. \\-]+)\n",
-    qemu64lmachineout,
-    flags=re.MULTILINE)
+    "^([\w\-\.]+)\s\s+([\w\(\)\+\,\. \-]+)\n", qemu64lmachineout, flags=re.MULTILINE)
 qemu64lmachinenlist = twolistize(qemu64lmachinelist)
 qemu64soundhwlistp = subprocess.Popen(
     [qemu64llocatout, '-soundhw', 'help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 qemu64lsoundhwout, qemu64lsoundhwerr = qemu64soundhwlistp.communicate()
 qemu64lsoundhwlist = re.findall(
-    "^([\\w]+)\\s\\s+([\\w\\(\\) ]+)\n", qemu64lsoundhwout, flags=re.MULTILINE)
+    "^([\w]+)\s\s+([\w\(\) ]+)\n", qemu64lsoundhwout, flags=re.MULTILINE)
 qemu64lsoundhwlist.append(('all', 'Enable all sound cards'))
 qemu64lsoundhwnlist = twolistize(qemu64lsoundhwlist)
-qemu64larglist = arglistize(
-    qemu64llocatout,
-    ('-hda',
-     '/dev/null'),
-    ('-cdrom',
-     '/home/cooldude2k/Emu/Minix3/minix_R3.3.0-588a35b.iso'),
-    ('-boot',
-     'order=acd,once=d,menu=on'),
-    ('-cpu',
-     qemu64lcpunlist['name']['reg'][9]),
-    ('-machine',
-     qemu64lmachinenlist['name']['reg'][17]),
-    ('-soundhw',
-     qemu64lsoundhwnlist['name']['reg'][8]),
-    ('-sdl',
-     None))
-print("PATH environment variable is set to " + os.environ["PATH"])
-print("Python " + sys.version)
-print(__project__ + " version " + __version__)
-print("QEMU emulator version " + qemu64lverfull)
+qemu64larglist = arglistize(qemu64llocatout, ('-hda', '/dev/null'), ('-cdrom', '/home/cooldude2k/Emu/Minix3/minix_R3.3.0-588a35b.iso'), ('-boot', 'order=acd,once=d,menu=on'),
+                            ('-cpu', qemu64lcpunlist['name']['reg'][9]), ('-machine', qemu64lmachinenlist['name']['reg'][17]), ('-soundhw', qemu64lsoundhwnlist['name']['reg'][8]), ('-sdl', None))
+print("PATH environment variable is set to "+os.environ["PATH"])
+print("Python "+sys.version)
+print(__project__+" version "+__version__)
+print("QEMU emulator version "+qemu64lverfull)
 print(qemu64larglist)
 qemu64cmdlaunch = subprocess.Popen(
     qemu64larglist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
